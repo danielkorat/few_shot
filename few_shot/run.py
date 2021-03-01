15b56213a9cd629c6d77825573a63f7eb1005850
Daniel Korat <daniel.korat@intel.com>
2021-03-01 17:13:59 +0200

    organize code

--- few_shot/run.py
+++ few_shot/run.py
@@ -3,12 +3,9 @@ from run_pattern_mlm import main as run_pattern_mlm
 import os
 
 def eval_pretrained():
-    pattern_kwargs = dict(pattern=P5, top_k=10)
-
-    data = load_all_datasets(train_size=200)
+    pattern_kwargs = dict(pattern=P5, top_k=5)
     # run_ds_examples(data['rest']['train'], model='roberta-base', **pattern_kwargs)
-
-    evaluate_all(data, lm='roberta-base', **pattern_kwargs)
+    evaluate_all(lm='roberta-base', **pattern_kwargs)
 
 def pattern_mlm_preprocess():
     data = load_all_datasets(train_size=200)
@@ -18,33 +15,39 @@ def pattern_mlm_preprocess():
     # Write splits to '/mlm_data'
     create_mlm_splits(data, P5)
 
-def main():
+def train_mlm(pattern, train_domain, seed=42, lr=1e-05, max_seq=256, max_steps=1000,
+            validation=None, model_type='roerta', model_name='roberta-base'):
 
-    # Pattern MLM training
-
-    # PET hparams: 
+    # hparams used in PET: 
     # lr", "1x10^-5, batch_size", "16, max_len", "256, steps", "1000
     # every batch: 4 labelled + 12 unlabelled
 
-    # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    # run_pattern_mlm([
-    #     "--pattern", P5,
-    #     "--seed", "42",
-    #     # "--num_train_epochs", "1",
-    #     "--learning_rate", "1e-05",
-    #     "--max_seq_length", "256",
-    #     "--max_steps", "1000",
-    #     "--validation_file", "few_shot/mlm_data/rest_test.txt",
-    #     # "--do_eval", "--validation_file", "few_shot/mlm_data/rest_test.txt", "--evaluation_strategy", "epoch",
-    #     "--line_by_line", "--output_dir", "pattern_mlm_model", "--train_file", "few_shot/mlm_data/lap_train.txt",
-    #     "--model_type", "roberta", "--model_name_or_path", "roberta-base",
-    #     "--do_train", "--overwrite_output_dir", "--overwrite_cache"])
+    output_dir = f"p-mlm_model_{train_domain}"
 
-    pattern_kwargs = dict(pattern=P5, top_k=10)
-    data = load_all_datasets(train_size=200)
-    evaluate_all(data, lm='pattern_mlm_model', **pattern_kwargs)    
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+    run_pattern_mlm([
+    "--pattern", pattern,
+    "--seed", str(seed),
+    # "--num_train_epochs", "1",
+    "--learning_rate", lr,
+    "--max_seq_length", max_seq,
+    "--max_steps", max_steps,
+    "--train_file", "few_shot/mlm_data/" + train_domain + '._train.txt',
+    # "--validation_file", "few_shot/mlm_data/" + validation,
+    # "--do_eval", "--validation_file", "few_shot/mlm_data/rest_test.txt",
+    # "--evaluation_strategy", "epoch",
+    "--line_by_line", "--output_dir", output_dir,
+    "--model_type", "roberta", "--model_name_or_path", "roberta-base",
+    "--do_train", "--overwrite_output_dir", "--overwrite_cache"])
+
+    return output_dir
+
+def main():
+    p_mlm_model = train_mlm(pattern = P5, train_domain='rest')
+    evaluate_all(lm=p_mlm_model, **dict(pattern=P5, top_k=5))    
 
 
 if __name__ == "__main__":
-    # main()
-    eval_pretrained()
+    main()
+    # eval_pretrained()
